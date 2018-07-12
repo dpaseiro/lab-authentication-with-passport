bbb2c82366f37630fa04b20e8df0fa3a1a12b59d
Daniel <paseiro101@yahoo.com>
2018-07-11 22:41:22 -0400

    MIA Daniel Paseiro

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -10,6 +10,65 @@ const passport      = require("passport");
 
 
 
-router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+// SIGNUP
+router.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
 });
+
+router.post('/signup', (req, res, next)=>{
+  const thePassword   = req.body.password;
+  const theUsername   = req.body.username;
+  
+  if(thePassword === "" || theUsername === ""){
+      res.render('passport/signup', {errorMessage: 'Please fill in both '});
+      return;
+  }
+
+User.findOne({username: theUsername})
+.then((responseFromDB)=>{
+  if(responseFromDB !== null){
+      res.render('passport/signup', {errorMessage: ` Someone already has ${theUsername}`})
+      return;
+  }
+  
+  
+  const salt          = bcrypt.genSaltSync(10);
+  const hashPassword  = bcrypt.hashSync(thePassword, salt);
+      User.create({username: theUsername, password: hashPassword})
+      .then((response)=>{
+          res.redirect('/login')
+      })
+      .catch((err)=>{
+          next(err)
+      });
+  })
+});
+
+// LOGIN
+router.get('/login', (req, res, next)=>{
+  res.render('passport/login', { message: req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private",
+  failureRedirect: "/signup",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+// PRIVATE
+router.get("/private", (req, res, next) => {
+  res.render("passport/private", {user: req,user});
+});
+
+
+
+
+// LOGOUT
+router.get("/logout", (req, res, next) => {
+    req.logout();
+    res.redirect("/passport/login");
+});
+
+
+module.exports = router;
